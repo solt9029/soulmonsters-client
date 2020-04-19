139d8a8b3bb0bea5b314731362f730c04c16d361
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-04-19 17:21:28 +0900

    Add apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import User from './containers/User';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export default function App() {
+const GET_CARDS = gql`
+  query {
+    cards {
+      id
+    }
+  }
+`;
+
+export function App(props: any) {
+  console.log(props);
   return (
     <div className="App">
       <User />
@@ -11,6 +23,23 @@ export default function App() {
         <Route exact path="/hello" component={() => <div>hello</div>} />
         <Route component={() => <div>notfound</div>} />
       </Switch>
+      <Query query={GET_CARDS}>
+        {(data: any) => {
+          if (data.error) {
+            return <div>error</div>;
+          }
+
+          if (data.loading) {
+            return <div>loading</div>;
+          }
+
+          return <div>data fetched!</div>;
+        }}
+      </Query>
     </div>
   );
 }
+
+export default graphql(GET_CARDS, {
+  options: { pollInterval: 1000 },
+})(App);
--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,84 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 import { ConnectedRouter } from 'connected-react-router';
-import { initializeApp } from 'firebase';
-import firebaseConfig from './config/firebase';
+import firebase, { initializeApp } from 'firebase';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import Lockr from 'lockr';
 
-initializeApp(firebaseConfig);
+const {
+  REACT_APP_HTTP_LINK_URI,
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
+export const ID_TOKEN = 'idtoken'; // local storage key name
+
+initializeApp({
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
+
+firebase.auth().onAuthStateChanged(async (user) => {
+  if (user) {
+    const idToken = await user.getIdToken(true);
+    Lockr.set(ID_TOKEN, idToken);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: REACT_APP_HTTP_LINK_URI,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const idToken = Lockr.get(ID_TOKEN);
+
+  // update id token every time
+  const newIdToken = await firebase.auth().currentUser?.getIdToken(true);
+  if (newIdToken) {
+    Lockr.set(ID_TOKEN, newIdToken);
+  }
+
+  return {
+    headers: {
+      ...headers,
+      authorization: idToken,
+    },
+  };
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistStore(store)}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </PersistGate>
-  </Provider>,
+  <ApolloProvider client={apolloClient}>
+    <Provider store={store}>
+      <PersistGate persistor={persistStore(store)}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </PersistGate>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
