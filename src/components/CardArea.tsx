237177089237b3aa19eb835bf041eab0fc218f70
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-05-08 09:29:10 +0900

    Add drop area

--- src/components/CardArea.tsx
+++ src/components/CardArea.tsx
@@ -3,12 +3,24 @@ import { Row, Col, UncontrolledAlert, Container } from 'reactstrap';
 import Card from '../components/Card';
 import { useCardsQuery } from '../graphql/generated/graphql-client';
 import Area from '../styled/Area';
+import * as AreaTypes from '../constants/area-types';
+import * as ItemTypes from '../constants/item-types';
+import { useDrop } from 'react-dnd';
 
 export default function CardArea() {
   const { data, error, loading } = useCardsQuery();
 
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: ItemTypes.DECK_CARD,
+    drop: () => ({ type: AreaTypes.CARD }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
   return (
-    <Area isRight>
+    <Area ref={drop} isActive={canDrop && isOver} isRight>
       <Container style={{ marginTop: '12px' }}>
         <Row style={{ color: 'white' }}>
           {loading && (
