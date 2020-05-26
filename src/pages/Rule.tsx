15ecd91fba9c4603902131a610b7248600435dd6
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-05-26 20:48:25 +0900

    Refactor

--- src/pages/Rule.tsx
+++ src/pages/Rule.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { ListGroup, ListGroupItem, Col } from 'reactstrap';
+import { Col } from 'reactstrap';
 import styled from 'styled-components';
 import Footer from '../components/Footer';
 import { Row, Container } from '../styled/reactstrap';
 
-const Title = styled.h3`
-  /* margin-left: 12px; */
-`;
-
-const SubTitle = styled.h5`
-  /* margin-left: 12px; */
-`;
-
-const Description = styled.div`
-  /* margin-left: 12px; */
-`;
-
 const Img = styled.img`
   width: 100%;
-  /* margin-left: 12px; */
 `;
 
 export default function Rule() {
@@ -27,7 +14,7 @@ export default function Rule() {
       <Container marginTop={12}>
         <Row marginBottom={50}>
           <Col lg={12}>
-            <Title>カードの種類</Title>
+            <h3>カードの種類</h3>
           </Col>
           <Col lg={12}>
             <ul>
@@ -65,159 +52,137 @@ export default function Rule() {
 
         <Row marginBottom={50}>
           <Col lg={12}>
-            <Title>ゾーンの種類</Title>
+            <h3>ゾーンの種類</h3>
           </Col>
           <Col md={6}>
             <img width="100%" src="/images/zone.png" alt="zone" />
           </Col>
           <Col lg={12}>
-            <SubTitle>①デッキゾーン</SubTitle>
+            <h5>①デッキゾーン</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>
-                  デッキを置く場所。デッキは40枚以上でなければならない。同名カードは3枚まで入れることができる。
-                </li>
-              </ul>
-            </Description>
+            <ul>
+              <li>
+                デッキを置く場所。デッキは40枚以上でなければならない。同名カードは3枚まで入れることができる。
+              </li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>②バトルゾーン</SubTitle>
+            <h5>②バトルゾーン</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>
-                  モンスターを置く場所。モンスターが戦闘で負けるとソウルゾーンに置かれる。モンスターがバトルゾーンからソウルゾーンに置かれたときエナジーが1増える。
-                </li>
-              </ul>
-            </Description>
+            <ul>
+              <li>
+                モンスターを置く場所。モンスターが戦闘で負けるとソウルゾーンに置かれる。モンスターがバトルゾーンからソウルゾーンに置かれたときエナジーが1増える。
+              </li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>③ソウルゾーン</SubTitle>
+            <h5>③ソウルゾーン</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>
-                  戦闘で負けたモンスターが置かれる場所。クイックカードやブロックカードを発動する場合、手札からソウルゾーンに置いて発動する。
-                </li>
-              </ul>
-            </Description>
+            <ul>
+              <li>
+                戦闘で負けたモンスターが置かれる場所。クイックカードやブロックカードを発動する場合、手札からソウルゾーンに置いて発動する。
+              </li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>④モルグゾーン</SubTitle>
+            <h5>④モルグゾーン</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>
-                  【ソウルキャノン】や【ソウルバリア】、コスト等でカードが置かれる場所。
-                </li>
-              </ul>
-            </Description>
+            <ul>
+              <li>
+                【ソウルキャノン】や【ソウルバリア】、コスト等でカードが置かれる場所。
+              </li>
+            </ul>
           </Col>
         </Row>
 
         <Row marginBottom={50}>
           <Col lg={12}>
-            <Title>ターンの流れ</Title>
+            <h3>ターンの流れ</h3>
           </Col>
           <Col lg={12}>
-            <SubTitle>1. ドロータイム</SubTitle>
+            <h5>1. ドロータイム</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>デッキからカードを1枚ドローする。</li>
-              </ul>
-            </Description>
+            <ul>
+              <li>デッキからカードを1枚ドローする。</li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>2. エナジータイム</SubTitle>
+            <h5>2. エナジータイム</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>エナジーを2増やす。エナジーの最大値は8。</li>
-              </ul>
-            </Description>
+            <ul>
+              <li>エナジーを2増やす。エナジーの最大値は8。</li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>3. プットタイム</SubTitle>
+            <h5>3. プットタイム</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>手札からカードを1枚までソウルゾーンに置くことができる。</li>
-              </ul>
-            </Description>
+            <ul>
+              <li>手札からカードを1枚までソウルゾーンに置くことができる。</li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>4. サムシングタイム</SubTitle>
+            <h5>4. サムシングタイム</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>
-                  【ソウルキャノン】ソウルゾーンのカードを4枚モルグゾーンに置き、相手のモンスター1枚をモルグゾーンに置くことができる。
-                </li>
-                <li>
-                  書かれている数字分エナジーを使ってモンスターカードを手札からバトルゾーンに置くことができる。
-                </li>
-                <li>
-                  サークルモンスターは、○で囲まれている数字の枚数自分のバトルゾーンに置かれているモンスターをモルグゾーンゾーンに置いて、手札からのみバトルゾーンに置くことができる。
-                </li>
-                <li>
-                  書かれている数字分エナジーを使ってクイックカードを手札からソウルゾーンに置き発動することができる。
-                </li>
-                <li>モンスターの表示形式を何度でも変更することができる。</li>
-              </ul>
-            </Description>
+            <ul>
+              <li>
+                【ソウルキャノン】ソウルゾーンのカードを4枚モルグゾーンに置き、相手のモンスター1枚をモルグゾーンに置くことができる。
+              </li>
+              <li>
+                書かれている数字分エナジーを使ってモンスターカードを手札からバトルゾーンに置くことができる。
+              </li>
+              <li>
+                サークルモンスターは、○で囲まれている数字の枚数自分のバトルゾーンに置かれているモンスターをモルグゾーンゾーンに置いて、手札からのみバトルゾーンに置くことができる。
+              </li>
+              <li>
+                書かれている数字分エナジーを使ってクイックカードを手札からソウルゾーンに置き発動することができる。
+              </li>
+              <li>モンスターの表示形式を何度でも変更することができる。</li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>5. バトルタイム</SubTitle>
+            <h5>5. バトルタイム</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>
-                  自分のモンスターで相手のモンスターを攻撃したり、相手を直接攻撃したりすることができる。
-                </li>
-                <li>
-                  【ソウルバリア】ソウルゾーンのカードを4枚モルグゾーンに置き、相手のモンスターからの攻撃を1回無効にすることができる。
-                </li>
-              </ul>
-            </Description>
+            <ul>
+              <li>
+                自分のモンスターで相手のモンスターを攻撃したり、相手を直接攻撃したりすることができる。
+              </li>
+              <li>
+                【ソウルバリア】ソウルゾーンのカードを4枚モルグゾーンに置き、相手のモンスターからの攻撃を1回無効にすることができる。
+              </li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>6. エンドタイム</SubTitle>
+            <h5>6. エンドタイム</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>
-                  ターンの終わり。エンドタイムが終わると相手のドロータイムとなる。
-                </li>
-              </ul>
-            </Description>
+            <ul>
+              <li>
+                ターンの終わり。エンドタイムが終わると相手のドロータイムとなる。
+              </li>
+            </ul>
           </Col>
           <Col lg={12}>
-            <SubTitle>7. 相手ターン</SubTitle>
+            <h5>7. 相手ターン</h5>
           </Col>
           <Col lg={12}>
-            <Description>
-              <ul>
-                <li>ブロックカードを発動することができる。 </li>
-              </ul>
-            </Description>
+            <ul>
+              <li>ブロックカードを発動することができる。 </li>
+            </ul>
           </Col>
         </Row>
 
         <Row marginBottom={50}>
           <Col lg={12}>
-            <Title>ルール</Title>
+            <h3>ルール</h3>
           </Col>
           <Col lg={12}>
             <ul>
