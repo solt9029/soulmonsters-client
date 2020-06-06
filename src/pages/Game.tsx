7427ce08b099fd94d36037377cf526e43a996ec0
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-06-06 17:38:01 +0900

    Design

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
-
 import { useActiveGameIdQuery } from '../graphql/generated/graphql-client';
 import { AppContext } from '../components/App';
+import { Container } from '../styled/reactstrap';
 
 export default function Game() {
   const { setActiveGameId } = useContext(AppContext);
@@ -16,16 +16,18 @@ export default function Game() {
 
   return (
     <>
-      {activeGameIdQueryResult.loading && <div>ロード中</div>}
-      {activeGameIdQueryResult.error !== undefined && (
-        <div>エラーが発生しました</div>
-      )}
-      {activeGameIdQueryResult.data?.activeGameId === null && (
-        <div>スタートゲーム！</div>
-      )}
-      {activeGameIdQueryResult.data?.activeGameId !== null && (
-        <div>ゲームの様子を描画するよ</div>
-      )}
+      <Container marginTop={12}>
+        {activeGameIdQueryResult.loading && <div>ゲーム情報をロード中です</div>}
+        {activeGameIdQueryResult.error !== undefined && (
+          <div>ゲーム情報の取得中にエラーが発生しました</div>
+        )}
+        {activeGameIdQueryResult.data?.activeGameId === null && (
+          <div>スタートゲーム！</div>
+        )}
+        {activeGameIdQueryResult.data?.activeGameId !== null && (
+          <div>ゲームの様子を描画するよ</div>
+        )}
+      </Container>
     </>
   );
 }
