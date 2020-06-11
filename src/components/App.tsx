a784e752291468434fb0a7b5b788af70f73d670b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-06-11 16:34:34 +0900

    Separate files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,39 +7,13 @@ import NotFound from '../pages/NotFound';
 import Index from '../pages/Index';
 import Deck from '../pages/Deck';
 import PrivateRoute from './PrivateRoute';
-import User from '../models/User';
 import { auth } from 'firebase';
 import { ID_TOKEN } from '../constants/local-storage-keys';
 import Lockr from 'lockr';
 import Game from '../pages/Game';
-import AppState, { ErrorName } from '../models/AppState';
-import { ApolloError } from 'apollo-client';
-import DeckModal from '../models/DeckModal';
-
-type Action =
-  | {
-      type: 'SET_USER';
-      payload: User;
-    }
-  | {
-      type: 'SET_SELECTED_DECK_ID';
-      payload: number | null;
-    }
-  | {
-      type: 'SET_ERROR';
-      payload: {
-        name: ErrorName;
-        error: ApolloError;
-      };
-    }
-  | {
-      type: 'RESET_ERROR';
-      payload: ErrorName;
-    }
-  | {
-      type: 'SET_DECK_MODAL';
-      payload: DeckModal;
-    };
+import AppState from '../models/AppState';
+import reducer from '../reducer';
+import Action from '../action';
 
 export interface AppContextInterface {
   state: AppState;
@@ -50,23 +24,6 @@ export const AppContext = createContext<AppContextInterface>({
   state: new AppState(),
   dispatch: () => {},
 });
-
-const reducer = (state: AppState, action: Action): AppState => {
-  switch (action.type) {
-    case 'SET_USER':
-      return state.setUser(action.payload);
-    case 'SET_SELECTED_DECK_ID':
-      return state.setSelectedDeckId(action.payload);
-    case 'SET_ERROR':
-      return state.setError(action.payload);
-    case 'RESET_ERROR':
-      return state.resetError(action.payload);
-    case 'SET_DECK_MODAL':
-      return state.setDeckModal(action.payload);
-    default:
-      return state;
-  }
-};
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, new AppState());
---
+++ src/reducer.ts
@@ -0,0 +1,19 @@
+import AppState from './models/AppState';
+import Action from './action';
+
+export default function (state: AppState, action: Action): AppState {
+  switch (action.type) {
+    case 'SET_USER':
+      return state.setUser(action.payload);
+    case 'SET_SELECTED_DECK_ID':
+      return state.setSelectedDeckId(action.payload);
+    case 'SET_ERROR':
+      return state.setError(action.payload);
+    case 'RESET_ERROR':
+      return state.resetError(action.payload);
+    case 'SET_DECK_MODAL':
+      return state.setDeckModal(action.payload);
+    default:
+      return state;
+  }
+}
