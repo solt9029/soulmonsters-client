3bc4d56485d78dc963da8e92889327a2a1155fd2
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-06-15 11:19:05 +0900

    Refactor

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -18,3 +18,17 @@ export const login = async (
     dispatch({ type: 'SET_USER', payload: user.failedLogin(error) });
   }
 };
+
+export const logout = async (
+  dispatch: Dispatch<Action>,
+  { user }: Pick<AppState, 'user'>
+) => {
+  dispatch({ type: 'SET_USER', payload: user.startLoading() });
+  try {
+    await auth().signOut();
+    dispatch({ type: 'SET_USER', payload: user.doneLogout() });
+    // TODO: initialize app state and clear graphql cache here.
+  } catch (error) {
+    dispatch({ type: 'SET_USER', payload: user.failedLogin(error) });
+  }
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,8 +17,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { AppContext } from './App';
-import { auth } from 'firebase';
-import { login } from '../actions/user';
+import { login, logout } from '../actions/user';
 
 const ServiceLogo = styled(NavbarBrand)`
   background: url('/images/icon.png') no-repeat left center;
@@ -78,15 +77,8 @@ export default function Navbar() {
     setIsDropdownOpen(false);
   }, [location.pathname]);
 
-  const logout = async () => {
-    dispatch({ type: 'SET_USER', payload: user.startLoading() });
-    try {
-      await auth().signOut();
-      dispatch({ type: 'SET_USER', payload: user.doneLogout() });
-      // TODO: initialize app state and clear graphql cache here.
-    } catch (error) {
-      dispatch({ type: 'SET_USER', payload: user.failedLogin(error) });
-    }
+  const handleLogoutClick = async () => {
+    await logout(dispatch, { user });
   };
 
   const handleLoginClick = async () => {
@@ -134,7 +126,9 @@ export default function Navbar() {
                 <Dropdown isOpen={isDropdownOpen} toggle={toggleDropdown}>
                   <UserLogo picture={user?.data?.photoURL} />
                   <DropdownMenu>
-                    <DropdownItem onClick={logout}>ログアウト</DropdownItem>
+                    <DropdownItem onClick={handleLogoutClick}>
+                      ログアウト
+                    </DropdownItem>
                   </DropdownMenu>
                 </Dropdown>
               );
