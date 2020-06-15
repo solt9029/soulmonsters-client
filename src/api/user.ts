9393bc4368286f9615abf99edfd8a7af870e93c9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-06-15 11:16:54 +0900

    Refactor

--- src/api/user.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from 'firebase';
-
-export const login = async () => {
-  return await auth().signInWithPopup(new auth.TwitterAuthProvider());
-};
-
-export const logout = async () => {
-  return await auth().signOut();
-};
