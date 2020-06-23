946e29e908cff110b7d6d34f4c0227fba94a62e4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-06-23 17:48:46 +0900

    Add content

--- src/components/game/MorgueGameCardListModal.tsx
+++ src/components/game/MorgueGameCardListModal.tsx
@@ -1,9 +1,18 @@
 import React, { useContext } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ListGroup,
+  ListGroupItem,
+} from 'reactstrap';
 import { AppContext } from '../App';
 import styled from 'styled-components';
 import { Row, Col } from '../../styled/reactstrap';
 import GameActionButton from './GameActionButton';
+import { BACK_SIDE_CARD } from '../../constants/pictures';
 
 const Img = styled.img`
   width: 100%;
@@ -24,8 +33,28 @@ export default function MorgueGameCardListModal() {
 
   return (
     <Modal isOpen={morgueGameCardListModal.isOpen} toggle={closeModal}>
-      <ModalHeader toggle={closeModal}>MorgueGameCardListModal</ModalHeader>
-      <ModalBody></ModalBody>
+      <ModalHeader toggle={closeModal}>
+        モルグゾーン情報（{morgueGameCardListModal.data.length}枚）
+      </ModalHeader>
+      <ModalBody>
+        <ListGroup>
+          {morgueGameCardListModal.data.map((gameCard) => (
+            <ListGroupItem>
+              <Row>
+                <Col lg={3} xs={3}>
+                  <Img src={gameCard.card?.picture || BACK_SIDE_CARD} />
+                </Col>
+                <Col lg={9} xs={9}>
+                  <h5>{gameCard.name}</h5>
+                  {gameCard.actionTypes.map((type) => (
+                    <GameActionButton type={type} gameCard={gameCard} />
+                  ))}
+                </Col>
+              </Row>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </ModalBody>
       <ModalFooter>
         <Button color="secondary" onClick={closeModal}>
           戻る
